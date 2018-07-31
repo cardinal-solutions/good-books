286d1e642c6aa491cce13730b9c1399920bfafdb
Chau Nguyen <cnguyen@cardinalsolutions.com>
2018-07-31 12:25:11 -0400

    This code provides result of api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { getThumbnails } from './api/thumbnailApi';
 import './App.css';
 
 class App extends Component {
+  renderThumbnails = () => {
+    getThumbnails().then(result => {
+      console.log('result: ', result);
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hello World</h1>
+
+        {this.renderThumbnails()}
       </div>
     );
   }
