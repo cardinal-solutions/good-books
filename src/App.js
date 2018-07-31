0957a24f79c1a113d40edb9261a13b4ed253c9b9
Chau Nguyen <cnguyen@cardinalsolutions.com>
2018-07-31 17:08:42 -0400

    Reverting App.js back to normal for consistency.
    Added @TODO to remove working codeblock reference

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import { renderThumbnail } from './api/helper';
 import './App.css';
 
 class App extends Component {
   render() {
-    const bookID = 'ISBN:0385472579';
-
     return (
       <div className="App">
         <h1>Hello World</h1>
-
-        {renderThumbnail(bookID)}
       </div>
     );
   }
--- src/api/helper.js
+++ src/api/helper.js
@@ -1,5 +1,11 @@
 import { getBookThumbnail } from './thumbnailApi';
 
+/* @TODO delete reference / mock data later
+  import { renderThumbnail } from './api/helper';
+  const bookID = 'ISBN:0385472579';
+  {renderThumbnail(bookID)}
+*/
+
 export const renderThumbnail = book => {
   getBookThumbnail(book).then(result => {
     console.log('result: ', result);
