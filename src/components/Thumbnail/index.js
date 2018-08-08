d9ce98c4771cf42c364e7fdb0d78a6ae9ae2c0ae
Chau Nguyen <cnguyen@cardinalsolutions.com>
2018-08-08 09:44:55 -0400

    Adding a more complete Book API end points

---
+++ src/components/Thumbnail/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Thumbnail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      thumbnailUrl: '',
+    };
+  }
+
+  render() {
+    const { thumbnailUrl } = this.state;
+    return (
+      <div className="Thumbnail">
+        <img src={thumbnailUrl} />
+      </div>
+    );
+  }
+}
+
+Thumbnail.propTypes = {
+  size: PropTypes.string,
+  cover: PropTypes.object,
+};
+
+Thumbnail.defaultProps = {
+  size: '',
+  cover: {},
+};
+
+export default Thumbnail;
