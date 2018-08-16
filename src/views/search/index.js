aa8138ec496b2f21370fdd0cdc8826bac8dd1d01
duggiemitchell <mitchell.erica31@gmail.com>
2018-08-16 10:29:32 -0400

    filtering books that have no image

--- src/views/search/index.js
+++ src/views/search/index.js
@@ -3,14 +3,18 @@ import SearchBar from '../../components/search-bar';
 import ListView from '../../components/list-view';
 // @todo: searchresults needs to be it's own <ListView /> component
 const SearchResults = ({ results }) => {
-  return results.map((book, idx) => (
-    <ListView
-      title={book.title_suggest}
-      author={book.author_name}
-      coverType="olid"
-      bookId={book.cover_edition_key}
-    />
-  ));
+  return results
+    .filter(
+      result => result.cover_edition_key !== undefined
+    )
+    .map((book, idx) => (
+      <ListView
+        title={book.title_suggest}
+        author={book.author_name}
+        coverType="olid"
+        bookId={book.cover_edition_key}
+      />
+    ));
 };
 // @todo:  Need to programmatically route to Search from any url
 class Search extends Component {
