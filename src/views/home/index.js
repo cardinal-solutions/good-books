ba080d42828c6466b2c9515f7151deac1c41778e
Chau Nguyen <cnguyen@cardinalsolutions.com>
2018-09-05 16:06:09 -0400

    Basic Grid

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
 // import Typography from '@material-ui/core/Typography';
 // import Divider from '@material-ui/core/Divider';
 
-const styles = {
+const styles = theme => ({
   root: {
     display: 'flex',
     justifyContent: 'center',
     flexWrap: 'wrap',
+    flexGrow: 1,
   },
-};
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
 
 class Home extends Component {
   state = {};
 
   render() {
-    return <div style={styles.root}>Home</div>;
+    const { classes } = this.props;
+    return (
+      <div style={styles.root}>
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>xs=12</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>xs=6</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>xs=6</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+        </Grid>
+      </div>
+    );
   }
 }
 
